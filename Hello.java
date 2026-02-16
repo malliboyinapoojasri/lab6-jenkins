fe92587d1208a101823d72995bf9306fa3e5c37a
MALLIBOYINA POOJA SRI <malliboyinapoojasri@gmail.com>
2026-02-16 15:33:13 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String args[]){
-        system.out.println("Hello Jeenkins from Java App");
+        System.out.println("Hello Jeenkins from Java App");
     }
 }
