866846f38e66d4a7a7436116825979137a95b9c1
MALLIBOYINA POOJA SRI <malliboyinapoojasri@gmail.com>
2026-02-16 15:55:34 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello{
     public static void main(String args[]){
+        int a=10;
+        int b=20;
+        System.out.println("Sum: "+(a+b));
         System.out.println("Hello Jenkins from Java App");
     }
 }
