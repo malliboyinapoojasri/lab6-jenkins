3c31630043f4fdada2493a856ea03d52bad128d1
MALLIBOYINA POOJA SRI <malliboyinapoojasri@gmail.com>
2026-02-17 19:20:21 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator{
+  public static void main(String args[]){
+    int a=10;
+    int b=20;
+    System.out.println("Addition is: "+(a+b));
+    System.out.println("Addition is: "+(b-a));
+    System.out.println("Addition is: "+(a*b));
+    System.out.println("Addition is: "+(b/a));
+  }
+}
+    
